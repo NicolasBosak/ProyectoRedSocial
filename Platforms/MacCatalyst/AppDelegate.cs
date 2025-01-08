3c747a18a47b5f0b0e837fcc2a2ba92129f5d7f7
Nicolas Casamen <nicolascasamen@outlook.com>
2025-01-08 16:00:40 -0500

    Add project files.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ProyectoRedSocial
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
